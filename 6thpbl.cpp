b5e4f5803d40e1066b82208a46e29ec4551a655d
parvesh <choudharyparvesht@gmail.com>
2026-06-07 19:45:40 +0530

    6

---
+++ 6thpbl.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int p, q;
+    p = 8, q = 3;
+
+    cout << "Program Initializing..." <<endl 
+    
+                                      <<endl;
+    
+    cout << "sum of p and q is: "<< (p+q) <<endl;
+    cout << "product of p and q is: "<< (p*q) <<endl;
+    cout << "difference of p and q is: "<< (p-q) <<endl;
+    cout << "modulo operation of p with q is: "<< (p%q) <<endl;
+    cout << "increment of q: "<< ++q <<endl;
+    cout << "decrement of q: "<< --q <<endl
+
+                                     <<endl;
+
+    cout << "Thank you for using this Program!";
+
+
+
+    return 0;
+}
--- start2.0.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int main(){
-
-    std::cout<<"it is very hot outside!";
-
-    return 0;
-
-}
