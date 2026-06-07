ca84ad74a6639673f2b57ed8eda384ea2ef0d857
parvesh <choudharyparvesht@gmail.com>
2026-06-07 21:16:18 +0530

    updated

---
+++ arithmatic_operations.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int p, q;
+    char x;
+   
+
+    cout << "Program Initializing..." <<endl 
+
+                                      <<endl;
+
+                                  
+
+    cout << "We are going to do sum, product, difference, module operation, increment and decrement of the two numbers, you have been asked" <<endl << endl;                                
+
+    cout << "Press any letter key to start" <<endl;
+    cin >> x;  
+
+    cout << "enter the value of 1st no, i.e. p" <<endl;
+    cin >> p;
+    cout << "enter the value of 2nd number, i.e. q" <<endl;
+    cin >> q;
+    
+    
+    
+    cout << "sum of p and q is: "<< (p+q) <<endl <<endl;
+    cout << "product of p and q is: "<< (p*q) <<endl <<endl;
+    cout << "difference of p and q is: "<< (p-q) <<endl <<endl;
+    cout << "modulo operation of p with q is: "<< (p%q) <<endl <<endl;
+    cout << "increment of p: "<< ++p <<endl <<endl;
+    cout << "increment of q: "<< ++q <<endl <<endl;
+    cout << "decrement of p: "<< --p <<endl <<endl;
+    cout << "decrement of q: "<< --q <<endl <<endl;
+
+    cout << "Arithmetic operations done sucessfully!" <<endl <<endl;
+
+    cout << "Press any letter key to end " <<endl;
+
+    cin >> x; 
+
+    cout << endl << "Thank you for using this Program!";
+
+
+
+    return 0;
+}
