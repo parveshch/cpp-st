703167247aa6b2ae1bc1dc0782abb204f4b2ee00
parvesh <choudharyparvesht@gmail.com>
2026-06-09 21:19:10 +0530

    using of while loop to create table

---
+++ multiplicationtable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int y;
+    char x;
+    y=0;
+    cout << "Program Initializing..." <<endl;
+    cout << "Table of 6" <<endl <<endl;
+
+   while (y>=0 && y<=20)
+   {
+     cout <<6<<'x'<<y<<"="<< y*(6) <<endl;
+
+    y++;
+   }
+   
+
+    return 0;
+}
+ 
