c9941bac5847f35d0fd847ca987b543cbbe39e63
parvesh <choudharyparvesht@gmail.com>
2026-06-09 14:57:44 +0530

    My first usable program.

--- bmicalculator.cpp
+++ bmicalculator.cpp
@@ -11,13 +11,13 @@ int main (){
     cout << "We are gonna calculate BMI"<<endl <<endl;
 
 
-    cout << "Enter your Weight in kg" <<endl;
+    cout << "Enter your Weight in kg-" <<endl;
     cin >> Weight;
 
-    cout << "Enter your Height in m" << endl;
+    cout << "Enter your Height in cm-" << endl;
     cin >> Height;
 
-    BMI = (Weight/(pow (Height, 2)));
+    BMI = (Weight/(pow ((Height/100), 2)));
 
     cout <<endl<< "Your BMI is: " << BMI << endl;
 
@@ -32,50 +32,50 @@ int main (){
     
     }
     else if(BMI >=18.5 && BMI <24.9){
-    cout << "You have a normal bodyweight."<<endl <<endl;
+    cout << "You have a healthy bodyweight."<<endl <<endl;
     
     }
     else if (BMI >24.9 && BMI <=29.9) {
-    cout << "You are overweight, No need to worry much, Just do exercise daily " <<endl <<endl;
+    cout << "You are overweight, No need to worry much, Just do exercise daily." <<endl <<endl;
     
     }
     else if (BMI > 29.9 && BMI <=34.9) {
-    cout << "You are obese Class - I, Cut out all the Junk food and run 10km daily" <<endl <<endl;
+    cout << "You have Class - I Obesity, Just cut out all the Junk food and run 10km daily." <<endl <<endl;
 
     }
     else if (BMI > 34.9 && BMI <=39.9) {
-    cout << "You are obese Class - II, Kindly consult a doctor!!!" <<endl <<endl;
+    cout << "You have Class - II Obesity, Kindly consult a doctor!!!" <<endl <<endl;
 
     }
     else if (BMI >= 40 && BMI <50) {
-    cout << "You are obese Class - III, Kindly consult a doctor!!!" <<endl <<endl;
+    cout << "You have Class - III Obesity, Kindly consult a doctor!!!" <<endl <<endl;
 
     }
 
     else if (BMI >= 50 && BMI <59.9) {
-    cout << "You have Class - IV Obesity, Kindly consult a doctor immediately!!!" <<endl <<endl;
+    cout << "You have Class - IV Obesity, Consult a doctor immediately!!!" <<endl <<endl;
 
     }
     else if (BMI >= 59.9 && BMI <69.9) {
-    cout << "You have Class - V Obesity, Kindly consult a doctor immediately!!!" <<endl <<endl;
+    cout << "You have Class - V Obesity, Consult a doctor immediately!!!" <<endl <<endl;
 
     }
     else if (BMI >= 69.9 && BMI <79.9) {
-    cout << "You have Class - VI Obesity, Kindly consult a doctor immediately!!!" <<endl <<endl;
+    cout << "You have Class - VI Obesity, Consult a doctor immediately!!!" <<endl <<endl;
 
     }
     else if (BMI >= 79.9 && BMI <=100) {
-    cout << "You have EXTREME Obesity, Kindly consult a doctor immediately!!!" <<endl <<endl;
+    cout << "You have EXTREME Obesity, Consult a doctor immediately!!!" <<endl <<endl;
 
     }
 
     else {
-    cout << BMI << " BMI is Beyond limits, kindly check your input values."<<endl <<endl;
+    cout << BMI << " BMI is Beyond limits, Kindly check your input values."<<endl <<endl;
     
     }
     
 
-    cout << "Thank you for using this Program!";
+    cout << "Thank you, Have a Good Day!";
 
     return 0;
 }
