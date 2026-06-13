e4a76271313baa62d2f0df26f8c91fea80ea4079
parvesh <choudharyparvesht@gmail.com>
2026-06-13 13:45:21 +0530

    right angled triangle via forloop

---
+++ forloop_program.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+    
+int i, j;
+
+for(i=1; i<=10; i++){
+
+
+for (j=1; j<=i; j++){
+cout <<'*';
+}
+
+cout <<endl;
+
+}
+
+return 0;
+}
+
